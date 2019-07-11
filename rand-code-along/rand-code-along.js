719ed2d3980bb7e7e51e020b9973e9c719abf85d
LeoEASC6 <leo.everth101@gmail.com>
2019-07-11 13:50:38 -0400

    functions

---
+++ rand-code-along/rand-code-along.js
@@ -0,0 +1,12 @@
+const multiplier = 100;
+
+function rand(word) {
+    const decimal = Math.random();
+    const times10 = decimal * multiplier;
+    const final = Math.floor(times10)
+    
+    return final + word;
+}
+
+
+console.log(rand(" times"));
